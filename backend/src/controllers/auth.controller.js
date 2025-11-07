563a2db976227341ca2b08032b5745d4ad1577df
sampath-create <sampath2005h@gmail.com>
2025-11-07 07:28:33 +0530

    signup code updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,8 +32,8 @@ export const signup = async (req,res) => {
         
 
         if (newUser){
-            generateToken(newUser._id,res);
-            await newUser.save();
+            const saveUser = await newUser.save();
+            generateToken(saveUser._id,res);
             res.status(201).json({
                 _id : newUser._id,
                 fullname : newUser.fullname,
