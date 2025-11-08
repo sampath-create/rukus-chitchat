a2d8b3d5058574469ae790dcd6c86c9018004b8a
sampath-create <sampath2005h@gmail.com>
2025-11-09 03:38:44 +0530

    messages-routing added

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,82 @@
+import Message from "../Models/message.js";
+import User from "../Models/User.js";
+
+export const getAllContacts = async (req ,res) => {
+    try{
+        const loggedInuserId = req.user._id
+        const fillteredusers = await User.find({ _id: { $ne: loggedInuserId } }).select("-password");
+        res.status(200).json(fillteredusers);
+    }
+    catch(error){
+        console.error("Error in getAllContacts controller:",error);
+        res.status(500).json({message : "Internal Server Error"})
+    }
+};
+export const getMessagesByUserId=async (req,res)=>{
+    try{
+        const myId = req.user._id;
+        const {id:usertochat}=req.params;
+        const messages= await Message.find({
+            $or : [
+                {senderId :myId , receiverId : usertochat},
+                {senderId :usertochat , receiverId : myId}
+            ]
+        })
+        res.status(200).json(messages);
+    }
+    catch(error){
+        res.status(500).json({message : "Internal Server Error"});
+        console.log(error);
+    }
+
+}
+export const sendMessage=async (req,res)=>{
+    try{
+        const {text , image} = req.body;
+        const {id:receiverId}=req.params;
+        const senderId = req.user._id;
+         let imageUrl;
+         if (image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl= uploadResponse.secure_url;
+         }
+
+         const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image : imageUrl
+         });
+         await newMessage.save();
+         //todo : send real time data through socket.io
+         res.status(201).json({message : "Message sent successfully"});
+    }
+    catch(error){
+        console.error("Error in sendMessage controller:",error);
+        res.status(500).json({message : "Internal Server Error"})
+    }
+}
+export const getChatPartners = async (req,res) =>{
+    try{
+        const loggedInuserId = req.user._id;
+        const messages = await Messages.find({
+            $or :[
+                {senderId : loggedInuserId},
+                {receiverId : loggedInuserId}   
+            ]
+        })
+        const ChatPartnerIds =[...new Set(messages.map((med) =>{
+            return med.senderId.toString() === loggedInuserId.toString() ? med.receiverId.toString() : med.senderId.toString();
+        }
+
+        ))];
+
+        const chatPartners =await User.find({_id : {$in : ChatPartnerIds}}).select("-password");
+        res.status(200).json(chatPartners);
+    }
+    catch(error){
+        console.error("Error in getChatPartners controller:",error);
+        res.status(500).json({message : "Internal Server Error"})
+    }
+
+}
