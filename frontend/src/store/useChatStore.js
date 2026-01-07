52339f00875480c25c8d2b79660afbdab21f4fa8
sampath-create <sampath2005h@gmail.com>
2026-01-07 22:13:57 +0530

    added all the required setup done in chat container

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -45,4 +45,18 @@ export const useChatStore = create((set, get)=>(
             set({isUserLoading:false});
         }
     },
-}));
+
+    getMessagesByUserId: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+    }
+)
+);
